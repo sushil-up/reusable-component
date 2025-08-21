8aea1cabda03357d73ecb0fdd69253d10dd158b7
sushil-up <107957800+sushil-up@users.noreply.github.com>
2025-08-21 15:43:10 +0530

    update

--- src/components/HomePageContent/HeroSection.jsx
+++ src/components/HomePageContent/HeroSection.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { BackgroundBeamsWithCollision } from "./backgroundbeam";
 import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 export function HeroSection() {
   const router = useRouter();
+  const [copied, setCopied] = useState(false);
+
   const hadleRedirect = () => {
     router.push("/login");
+  };
+  const commandInstall = `npx shadcn@latest init`;
+  const handleCommandCode = async () => {
+    await navigator.clipboard.writeText(commandInstall);
+    toast.success("Copied to clipboard!");
+    setTimeout(() => setCopied(false), 2000);
   };
   return (
     <BackgroundBeamsWithCollision>
@@ -16,8 +28,37 @@ export function HeroSection() {
             <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-0">
               <span className="">Open Source.</span>
             </div>
+            <span></span>
           </div>
         </h2>
+        <div className="relative w-full max-w-lg mx-auto">
+  <div className="relative rounded-lg overflow-hidden border border-slate-800 bg-slate-950">
+    {/* Copy Button */}
+    <Button
+      onClick={handleCommandCode}
+      className="absolute right-2 top-2 z-10 cursor-pointer"
+      variant="secondary"
+      size="sm"
+    >
+      {copied ? "Copied!" : "Copy"}
+    </Button>
+
+    {/* Code Block */}
+    <SyntaxHighlighter
+      language="bash"
+      style={atomOneDark}
+      customStyle={{
+        margin: 0,
+        padding: "1rem 1.5rem",
+        fontSize: "0.9rem",
+        background: "transparent",
+      }}
+    >
+      {commandInstall}
+    </SyntaxHighlighter>
+  </div>
+</div>
+
         <div className="flex flex-col sm:flex-row justify-center gap-4 w-[60%] mx-auto py-2">
           <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
