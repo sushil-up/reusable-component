c8971322de5a1345644eda0c8fdb8f9303382d50
sushil-up <107957800+sushil-up@users.noreply.github.com>
2025-08-25 17:29:50 +0530

    `Fixed whitespace in JSX component: removed unnecessary newline characters`

--- src/app/admin/components/toaster/page.jsx
+++ src/app/admin/components/toaster/page.jsx
@@ -11,15 +11,15 @@ export const ToolTipCode=()=>{
     import { toast } from "sonner";
     import { Button } from "@/components/ui/button";
     return(
-    <>
+    <> 
           <Button
-                        variant="outline"
-                        onClick={() =>
-                            toast("Event has been created", {
-                                description: "Sunday, December 03, 2023 at 9:00 AM",
-                                action: {
-                                    label: "Undo",
-                                    onClick: () => console.log("Undo"),
+                  variant="outline"
+                      onClick={() =>
+                       toast("Event has been created", {
+                          description: "Sunday, December 03, 2023 at 9:00 AM",
+                            action: {
+                                label: "Undo",
+                                onClick: () => console.log("Undo"),
                                 },
                             })
                         }
